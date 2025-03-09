a214e780761026de00a0f6fdb366f0949c379291
Radu <obs@live.it>
2025-03-10 00:37:30 +0100

    Added first example

---
+++ anna/vettori/esempio_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numeri[5]; // Creo un array di 5 interi
+
+    // Assegno valori alle 5 posizioni
+    numeri[0] = 10;
+    numeri[1] = 20;
+    numeri[2] = 30;
+    numeri[3] = 40;
+    numeri[4] = 50;
+
+    // stampo i valori
+    cout << "Il primo numero è: " << numeri[0] << endl;
+    cout << "Il terzo numero è: " << numeri[2] << endl;
+
+    return 0;
+}
