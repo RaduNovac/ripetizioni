596801ca9193888f916bad5ba03da867e4b40050
Radu <obs@live.it>
2025-04-24 14:11:21 +0200

    added new things

---
+++ alessandro/esercizi/base.cpp
@@ -0,0 +1,14 @@
+/* 
+    g++ -std=c++11 base.cpp -o base
+
+    ./base
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
---
+++ alessandro/esercizi/tmp.c++
@@ -0,0 +1,14 @@
+/* 
+    g++ -std=c++11 tmp.cpp -o tmp
+
+    ./tmp
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
