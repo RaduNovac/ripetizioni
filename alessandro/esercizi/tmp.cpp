8fd2437128631932afbc32cf5313ea0d4eec31dd
Radu <obs@live.it>
2025-05-14 12:19:45 +0200

    update

--- alessandro/esercizi/tmp.cpp
+++ alessandro/esercizi/tmp.cpp
@@ -7,33 +7,98 @@
 
 using namespace std;
 
-int main(){
-    int n = 7;
-
-    string giorni_settimana[n];
-
-    giorni_settimana[0] = "lunedì";
-    giorni_settimana[1] = "martedì";
-    giorni_settimana[2] = "mercoledì";
-    giorni_settimana[3] = "giovedì";
-    giorni_settimana[4] = "venerdì";
-    giorni_settimana[5] = "sabato";
-    giorni_settimana[6] = "domenica";
-    /*
-    1: la variabile che usi per fare il ciclo
-    2: fino a dove vuoi arrivare
-    3: come modifichi la variabile
-    */
-    for(int i = 0;i < n;i = i + 2){
-        cout << giorni_settimana[i] << endl;
+void stampa_fino_a_dispari(int v[], int dim){
+    for(int i = 0; i < dim; i++){
+        if(v[i] % 2 != 0){//il numero è dispari
+            return;
+        }
+        else{
+            cout << v[i] << endl;
+        }
     }
-    
-    cout << sizeof(giorni_settimana) << endl;
-    cout << sizeof(giorni_settimana[0]) << endl;
-    
-    int dimensione = sizeof(giorni_settimana) / sizeof(giorni_settimana[0]);
+}
 
-    cout << "dimensione vettore: " << dimensione << endl;
+void stampa_ciao(){
+	cout << "Ciao!" << endl;
+    return;
+    cout << "Ciaone!" << endl;
+    cout << "haha" << endl;
+}
 
-    return 0;
+int somma(int a, int b) {   // ritorna un int, riceve due int come parametri
+    return a + b;           // restituisce la somma dei due numeri
+}
+
+bool numero_pari(int n){
+    bool risultato = false;
+
+    if(n % 2 == 0){
+        risultato = true;
+    }
+
+    return risultato;
+}
+
+void incrementa(int &n){
+    n = n + 5;
+
+    cout << "dentro la funzione: " << n << endl;
+}
+
+void scambia(int &x, int &y){
+	int z;
+	
+	z = x;
+	x = y;
+	y = z;
+
+    cout << "nella funzione = x: " << x << ", y: " << y << endl;
+}
+
+void stampa_array(int v[]){
+	int i;
+	
+	for(i = 0;i < 5;i++){
+		cout << v[i] << endl;
+	}
+}
+
+void trasla(int v[]){
+    int temporanea = v[0];
+
+    for(int i = 0; i < 4; i++){
+       /* cout << "i: " << i << endl;
+        cout << "v[i]: " << v[i] << endl;
+        cout << "v[i + 1]: " << v[i + 1] << endl;*/
+        v[i] = v[i + 1];
+    }
+
+    v[4] = temporanea;
+}
+
+/*
+    Creare una funzione che calcola e stampa a schermo il valore assoluto di un numero intero.
+*/
+
+void valore_assoluto(int numero){
+    int risultato;
+
+    if(numero < 0){//negativo
+        risultato = numero * -1;
+        cout << "a" << endl;
+    }
+    else{
+        risultato = numero;
+        cout << "b" << endl;
+    }
+
+    cout << risultato << endl;
+}
+
+int main(){
+    //int x = valore_assoluto(-23);
+
+    //cout << x << endl;
+
+	return 0;
 }
